0aa083b8ef423ec5efedf6d5d1b3775dbad6d432
winniez <xinyingzengvy@gmail.com>
2014-04-26 02:14:21 -0600

    Initial Commit

---
+++ SnowGlobe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SnowGlobe
+//
+//  Created by Winnie Zeng on 4/26/14.
+//  Copyright (c) 2014 Winnie Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SnowGlobe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SnowGlobe
+//
+//  Created by Winnie Zeng on 4/26/14.
+//  Copyright (c) 2014 Winnie Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
